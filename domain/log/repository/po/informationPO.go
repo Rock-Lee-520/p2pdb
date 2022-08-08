d696d614af49c2f6a77a70cc122eb81571c6715c
rock <383329495@qq.com>
2022-08-08 21:29:00 +0800

    add log

--- domain/log/repository/po/informationPO.go
+++ domain/log/repository/po/informationPO.go
@@ -1,1 +1,32 @@
 package po
+
+// Node  information_schema definition
+type Node struct {
+	BaseColumn
+	NodeId       string `gorm:"column:node_id"`
+	RequestId    string `gorm:"column:request_id"`
+	Type         string `gorm:"column:type"`
+	LogicalClock string `gorm:"column:logcal_clock"`
+	LastNodeId   string `gorm:"column:last_node_Id"`
+	DataIds      string `gorm:"column:data_ids"`
+}
+
+// Data  information_schema definition
+type Data struct {
+	BaseColumn
+	DataId    string `gorm:"column:data_id"`
+	Operation string `gorm:"column:operation"`
+	Propertie string `gorm:"column:propertie"`
+}
+
+// Link  information_schema definition
+type Link struct {
+	BaseColumn
+	LinkId     string `gorm:"column:link_id"`
+	LastNodeId string `gorm:"column:last_node_id"`
+	NodeId     string `gorm:"column:node_id"`
+	TableId    string `gorm:"column:table_id;primary_key"`
+	DatabaseId string `gorm:"column:database_id"`
+	InstanceId string `gorm:"column:local_instance_id"`
+	Size       int64  `gorm:"column:size"`
+}
--- domain/store/repository/po/informationPO.go
+++ domain/store/repository/po/informationPO.go
@@ -3,22 +3,19 @@ package po
 // Database  information_schema definition
 type DatabaseInfomation struct {
 	BaseColumn
-	DatabaseId      string `gorm:"column:database_id"`
+	DatabaseId      string `gorm:"column:database_id;primary_key"`
 	DatabaseName    string `gorm:"column:database_name"`
-	TableId         string `gorm:"column:table_id"`
-	LocalPeerId     string `gorm:"column:local_peer_id"`
 	LocalInstanceId string `gorm:"column:local_instance_id"`
 }
 
 // Table  information_schema definition
 type TableInfomation struct {
 	BaseColumn
-	TableId         string `gorm:"column:table_id"`
+	TableId         string `gorm:"column:table_id;primary_key"`
 	DatabaseId      string `gorm:"column:database_id"`
 	TableName       string `gorm:"column:table_name"`
-	LocalPeerId     string `gorm:"column:local_peer_id"`
 	LocalInstanceId string `gorm:"column:local_instance_id"`
-	ClockTime       int64  `gorm:"column:clock_time"`
+	LogicalClock    int64  `gorm:"column:clock_time"`
 	Version         int64  `gorm:"column:version"`
 	Schema          int64  `gorm:"column:schema"`
 }
