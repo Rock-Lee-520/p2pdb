0b87fbea13398fed57f29309872524661d21a1cb
rock <383329495@qq.com>
2022-08-11 22:00:00 +0800

    update stroage_infrastructure

--- infrastructure/core/server/stroage_infrastructure.go
+++ infrastructure/core/server/stroage_infrastructure.go
@@ -11,14 +11,21 @@ import (
 func CreateSqliteDatabase(DBName string, tableName string) *sqlite.Database {
 
 	db := sqlite.NewDatabase(DBName)
-	tableName = "test2"
 	db.InitTables()
-	// table := sqlite.NewTable(tableName, sql.PrimaryKeySchema{})
-	// // debug.Dump(table)
+	// table := sqlite.NewTable(tableName, sql.NewPrimaryKeySchema(sql.Schema{
+	// 	{Name: "name2", Type: sql.Text, Nullable: false, Source: tableName},
+	// 	{Name: "email2", Type: sql.Text, Nullable: false, Source: tableName},
+	// 	{Name: "id", Type: sql.Int64, Nullable: false, Source: tableName},
+	// 	// {Name: "phone_numbers", Type: sql.JSON, Nullable: false, Source: tableName},
+	// 	// {Name: "created_at", Type: sql.Timestamp, Nullable: false, Source: tableName},
+	// }))
+	// // // debug.Dump(table)
 	// db.AddTable(tableName, table)
+
 	// ctx := sql.NewEmptyContext()
-	// table.Insert(ctx, sql.NewRow("John Doe", "john@doe.com", 1))
-	// table.Insert(ctx, sql.NewRow("John Doe", "john@doe.com", 2))
+	// err := table.ApplyEdits(ctx)
+	// log.Debug(err)
+	//table.Insert(ctx, sql.NewRow("John Doe", "john@doe.com", 2))
 	// table.Insert(ctx, sql.NewRow("John Doe", "johnalt@doe.com", []string{}, time.Now()))
 	// table.Insert(ctx, sql.NewRow("Jane Doe", "jane@doe.com", []string{}, time.Now()))
 	// table.Insert(ctx, sql.NewRow("Evil Bob", "evilbob@gmail.com", []string{"555-666-555", "666-666-666"}, time.Now()))
