8954ed1ff1f51a377305d5e1f2bdcad61c89544c
rock <383329495@qq.com>
2022-10-01 23:03:03 +0800

    add GetSnowflakeId method

--- infrastructure/util/function/local.go
+++ infrastructure/util/function/local.go
@@ -1,8 +1,34 @@
 package function
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/snowflake"
 	uuid "github.com/satori/go.uuid"
 )
+
+var node *snowflake.Node
+
+func GetSnowflakeId() int64 {
+	var st time.Time
+
+	// 格式化 1月2号下午3时4分5秒  2006年
+	st, err := time.Parse("2006-01-02", time.Now().Format("2006-01-02"))
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	snowflake.Epoch = st.UnixNano() / 1e6
+	node, err = snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		return 2
+	}
+
+	return node.Generate().Int64()
+}
 
 func GetLocalHost() string {
 	return "127.0.0.1"
